84447b1878ae3beae10a60d2fd882a1d3a7974a1
imane <benbenimane@hotmail.com>
2020-05-16 20:29:20 +0200

    responsiveness, adding icons, editing card...

--- src/components/card.js
+++ src/components/card.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import "../Assets/css/card.scss"
+import {FaGithub} from "react-icons/all";
 
-function Card({projectName,projectDescription,projectLink}) {
+import {IconContext} from "react-icons";
+
+function Card({projectName,projectDescription,projectLink,GithubLink}) {
 
     return(
-        <a target="_blank" rel="noopener noreferrer"  href={projectLink}>
+
         <div className={'card'} >
-            <h3>{projectName}</h3>
+            <div>
+
+                <ul>
+                    <li>
+                        <a target="_blank" rel="noopener noreferrer"  href={projectLink}>
+                            <h3>{projectName}</h3>
+                        </a>
+                    </li>
+                    <li>
+                        <a target="_blank" rel="noopener noreferrer"  href={GithubLink}>
+                        <IconContext.Provider value={{ style: {fontSize: '20px',color:"white"}}}>
+                            <FaGithub/>
+                        </IconContext.Provider>
+                        </a>
+                    </li>
+                </ul>
+
+            </div>
+
             <h5>{projectDescription}</h5>
         </div>
-        </a>
+
     )
 
 }
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {IconContext} from "react-icons"
-import { FaLinkedin, FaInstagram, FaGithub, FaDribbble} from "react-icons/fa"
+import { FaLinkedin, FaInstagram, FaGithub, FaDribbble,FaPhp,FaSymfony,FaHtml5,FaCss3,FaDatabase,FaSass,FaJs,FaReact} from "react-icons/fa"
+
 
 import work10 from "../img/work10.png"
 import eyes from "../img/eyes.svg"
@@ -18,78 +19,119 @@ const Main = () =>{
             <Nav className={"nav-dark"}/>
             <div className={"container-intro"}>
                 <div className={"intro-text"}>
-                   <h1>Hi, I'm Imane </h1>
+                    <h1>Hi, I'm Imane </h1>
                     <p> I design, code and draw</p>
-                  <button className="intro-button"><a href="#footer">Get in touch </a></button>
-                  <button className="intro-button"><Link to={"/projects"}>Browse projects </Link></button>
+                    <button className="intro-button"><a href="#footer">Get in touch </a></button>
+                    <button className="intro-button"><Link to={"/projects"}>Browse projects </Link></button>
                 </div>
                 <div className={"intro-social"}>
-                       <ul>
+                    <ul>
 
-                           <li>
-                               <a  target="_blank" rel="noopener noreferrer" href={"https://github.com/bennami"}>
-                                   <IconContext.Provider value={{ style: {fontSize: '25px', color: "black"}}}>
-                                   <FaGithub/>
-                                   </IconContext.Provider>
-                               </a>
-                           </li>
-                           <li><p>Github</p></li>
-                       </ul>
-                       <ul>
-                           <li>
-                               <a  target="_blank" rel="noopener noreferrer" href={"https://www.linkedin.com/in/imane-benyecif-k-87099073/"}>
-                                   <IconContext.Provider value={{ style: {fontSize: '25px', color: "black"}}}>
-                                   <FaLinkedin/>
-                                   </IconContext.Provider>
-                                </a>
-                           </li>
-                          <li> <p>Linkedin</p></li>
-                       </ul>
-                       <ul>
-                           <li>
-                               <a   target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/bennami_/">
-                                   <IconContext.Provider value={{ style: {fontSize: '25px', color: "black"}}}>
-                                   <FaInstagram/>
-                                   </IconContext.Provider>
-                               </a>
-                           </li>
-                          <li> <p>Instagram</p></li>
-                       </ul>
-                       <ul>
-                           <li>
-                               <a   target="_blank" rel="noopener noreferrer" href="https://dribbble.com/bennami">
-                                   <IconContext.Provider value={{ style: {fontSize: '25px', color: "black"}}}>
-                                       <FaDribbble/>
-                                   </IconContext.Provider>
-                               </a>
-                           </li>
-                           <li> <p>Dribble</p></li>
-                       </ul>
-                   </div>
-               </div>
+                        <li>
+                            <a  target="_blank" rel="noopener noreferrer" href={"https://github.com/bennami"}>
+                                <IconContext.Provider value={{ style: {fontSize: '25px', color: "black"}}}>
+                                    <FaGithub/>
+                                </IconContext.Provider>
+                            </a>
+                        </li>
+                        <li><p>Github</p></li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <a  target="_blank" rel="noopener noreferrer" href={"https://www.linkedin.com/in/imane-benyecif-k-87099073/"}>
+                                <IconContext.Provider value={{ style: {fontSize: '25px', color: "black"}}}>
+                                    <FaLinkedin/>
+                                </IconContext.Provider>
+                            </a>
+                        </li>
+                        <li> <p>Linkedin</p></li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <a   target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/bennami_/">
+                                <IconContext.Provider value={{ style: {fontSize: '25px', color: "black"}}}>
+                                    <FaInstagram/>
+                                </IconContext.Provider>
+                            </a>
+                        </li>
+                        <li> <p>Instagram</p></li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <a   target="_blank" rel="noopener noreferrer" href="https://dribbble.com/bennami">
+                                <IconContext.Provider value={{ style: {fontSize: '25px', color: "black"}}}>
+                                    <FaDribbble/>
+                                </IconContext.Provider>
+                            </a>
+                        </li>
+                        <li> <p>Dribble</p></li>
+                    </ul>
+                </div>
+            </div>
 
             <main>
                 <section className="services">
                     <div className="service-container1">
                         <div className={"cards"}>
-                        <Card projectName={"weather app"}
-                              projectDescription={" a beautifully designed site to get the weather for the upcoming 5 days"}
-                              projectLink={"https://bennami.github.io/weather-API/"}
+                            <Card projectName={"weather app"}
+                                  projectDescription={" a beautifully designed site to get the weather for the upcoming 5 days"}
+                                  projectLink={"https://bennami.github.io/weather-API/"}
                             />
-                        <Card projectName={"Poke-dex"}
-                              projectDescription={"The classic poke-dex from the pokemon series in a web app! browse through the entire library of pokemon thanks to the PokeApi"}
-                              projectLink={"https://bennami.github.io/weather-API/"}
-                        />
-                        <Card projectName={"Movie DB"}
-                              projectDescription={"This websites features a movie Api that lets you browse through a huge library of movies"}
-                              projectLink={"https://bennami.github.io/Movie-browse/#/"}
-                        />
+                            <Card projectName={"Poke-dex"}
+                                  projectDescription={"The classic poke-dex from the pokemon series in a web app! browse through the entire library of pokemon thanks to the PokeApi"}
+                                  projectLink={"https://bennami.github.io/weather-API/"}
+                            />
+                            <Card projectName={"Movie DB"}
+                                  projectDescription={"This websites features a movie Api that lets you browse through a huge library of movies"}
+                                  projectLink={"https://bennami.github.io/Movie-browse/#/"}
+                            />
                         </div>
                     </div>
 
                     <div className="service-container2"  id={"about"}>
+                        <ul>
+                            <li>
+                                <IconContext.Provider value={{ style: {fontSize: '25px'}}}>
+                                    <FaHtml5/>
+                                </IconContext.Provider>
+                            </li>
+                            <li>
+                                <IconContext.Provider value={{ style: {fontSize: '25px'}}}>
+                                    <FaPhp/>
+                                </IconContext.Provider></li>
+                            <li>
+                                <IconContext.Provider value={{ style: {fontSize: '25px'}}}>
+                                    <FaJs/>
+                                </IconContext.Provider>
+                            </li>
+                            <li>
+                                <IconContext.Provider value={{ style: {fontSize: '25px'}}}>
+                                    <FaCss3/>
+                                </IconContext.Provider>
+                            </li>
+                            <li>
+                                <IconContext.Provider value={{ style: {fontSize: '25px'}}}>
+                                    <FaSass/>
+                                </IconContext.Provider>
+                            </li>
+                            <li>
+                                <IconContext.Provider value={{ style: {fontSize: '25px'}}}>
+                                    <FaDatabase/>
+                                </IconContext.Provider>
+                            </li>
+                            <li>
+                                <IconContext.Provider value={{ style: {fontSize: '25px'}}}>
+                                <FaReact/>
+                                </IconContext.Provider>
+                            </li>
+                            <li>
+                                <IconContext.Provider value={{ style: {fontSize: '25px'}}}>
+                                    <FaSymfony/>
+                                </IconContext.Provider>
+                            </li>
+                        </ul>
                         <div className="service-description">
-                            <div className={"text"}>Hi! My name is Imane. I am an upcoming web developer and graphic designer. I graduated in 2017 from Saint Lucas Antwerp (as a graphic designer). Since then, I went on and acquired a second degree in Art education and I am currently enrolled in a super intense web development course. My work is a combination of analogue and digital  tools, as well as socially relevant topics. I love to work on projects that involve different mediums such as analogue printing and new digital tools. Right now I am focusing on front end frameworks such as React JS, but I have worked with Symfony PHP for back end projects, MySQL and many more. <span>
+                            <div className={"text"}>Hi! I'm Imane. I am a web developer and graphic designer. I graduated in 2017 as a graphic designer. Since then, I went on and acquired a second degree in Art education and I am currently enrolled in a super intense web development course.<br/><br/> My work is a combination of analogue and digital  tools, as well as socially relevant topics. I love to work on projects that involve different mediums such as analogue printing and new digital tools. Right now I am focusing on front end frameworks such as React JS, but I have worked with Symfony <FaPhp/> for back end projects, MySQL and many more.<br/><br/> <span>
                                 <Link to={"/projects"}>
                                     Check out some of my work!
                                 </Link>
@@ -108,7 +150,7 @@ const Main = () =>{
                     </div>
                 </section>
             </main>
-           <Footer/>
+            <Footer/>
         </>
     )
 
